795c277e3870413a65b1e4885448f115ce02c7c8
Mengyun Yang <45998351+YangPhD84@users.noreply.github.com>
2025-03-03 18:31:50 +0800

    Update fDRDMF.m

--- code/fDRDMF.m
+++ code/fDRDMF.m
@@ -7,10 +7,9 @@ function [M, Z, W] = fDRDMF(A,data,data_Similarity,layers,alpha,beta,lammad,gamm
 num_of_layers = numel(layers);
 numOfView = numel(data);
 
-%initialize—基于原始图的拉普拉斯矩阵L_W
 K_W = cell(1,numOfView);
 O_W = cell(1,numOfView);
-L_W = cell(1,numOfView);%构建关于基矩阵W的拉普拉斯矩阵L_W
+L_W = cell(1,numOfView);
 
 for v_ind = 1:numOfView
     K_W{1,v_ind} = diag(sum(data_Similarity{v_ind},2));
@@ -37,7 +36,6 @@ for iter = 1:MaxIter
         end
      end
     
-     %得出最终关联矩阵
     [row,col] = size(data{1,1});
     M = zeros(row,col);
     for i = 1:numOfView
@@ -49,8 +47,7 @@ for iter = 1:MaxIter
         M = M + M_result{i,1};
     end
     M = M./numOfView;
-     %% 和multiGMF同样的判定迭代终止条件
-     if Dataset == 1
+    if Dataset == 1
          if T == 1
              stop1_0 = stop1;
              stop1 = norm(M(:,594:end)'-WH,'fro')/norm(WH,'fro');
